7473b5c69f822c53a7695e43d135c7681a1cf3bf
Swietoslaw <0.patrykmalinowski@gmial.com>
2022-06-20 11:21:56 +0200

    Angular update

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -54,7 +54,7 @@ namespace API.Controllers
 
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName); //UserName
 
             if (user == null) return Unauthorized("Invalid username");
 
--- API/DTOs/LoginDto.cs
+++ API/DTOs/LoginDto.cs
@@ -7,7 +7,7 @@ namespace API.DTOs
 {
     public class LoginDto
     {
-        public string UserName { get; set; }
+        public string UserName { get; set; } //UserName
         public string Password { get; set; }
     }
 }
