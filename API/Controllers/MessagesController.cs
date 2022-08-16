ff42101e2dc85feb3efbbac1072e5b259b3a32e9
Swietoslaw <0.patrykmalinowski@gmial.com>
2022-08-16 12:58:04 +0200

    Deleting messages on the API

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -71,6 +71,21 @@ namespace API.Controllers
 
             return Ok(await _messageRepository.GetMessagesThread(currentUserName, username));
         }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteMessage(int id)
+        {
+            var username = User.GetUsername();
 
+            var message = await _messageRepository.GetMessage(id);
+
+            if (message.Sender.UserName != username && message.Recipient.UserName != username) 
+                return Unauthorized();
+
+            if (message.Sender.UserName == username) message.SenderDeleted = true;
+            if (message.Recipient.UserName == username) message.RecipientDeleted = true;
+            if (message.SenderDeleted && message.RecipientDeleted) _messageRepository.DeleteMessage(message);
+            if (await _messageRepository.SaveAllAsync()) return Ok();
+            return BadRequest("Problem z usuwaniem wiadomosci");
+        }
     }
 }
