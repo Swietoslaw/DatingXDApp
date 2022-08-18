01d83d1becbc53a52fa0f6f52be7b7f44a267660
Swietoslaw <0.patrykmalinowski@gmial.com>
2022-08-18 11:08:47 +0200

    Identity, role menagment, setting up the entities

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -30,11 +30,7 @@ namespace API.Controllers
 
             var user = _mapper.Map<AppUser>(registerDto);
 
-            using var hmac = new HMACSHA512();
-
             user.UserName = registerDto.UserName.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            user.PasswordSalt = hmac.Key;
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
@@ -57,16 +53,7 @@ namespace API.Controllers
             .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName); //UserName
 
             if (user == null) return Unauthorized("Invalid username");
-
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int i = 0; i < computedHash.Length; i++)
-            {
-                if (computedHash[i] != user.PasswordHash[i] ) return Unauthorized("Invalid password");
-            }
-
+            
             return new UserDto
             {
                 UserName = user.UserName,
