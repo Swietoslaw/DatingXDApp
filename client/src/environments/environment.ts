98b5574bf4e87457ce28857b49ea8f52a427dc33
Swietoslaw <0.patrykmalinowski@gmial.com>
2022-08-28 17:06:46 +0200

    Client side SignalR

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl:"https://localhost:5003/api/"
+  apiUrl:"https://localhost:5003/api/",
+  hubUrl:"https://localhost:5003/hubs/"
 };
 
 /*
